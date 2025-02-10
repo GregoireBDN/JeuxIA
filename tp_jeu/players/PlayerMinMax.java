c0c40a17b67eb1be5f43305a39271e971795e1a1
GregoireBDN <gregoire.bodin04@gmail.com>
2025-02-10 16:42:16 +0100

    seance 1

--- tp_jeu/players/PlayerMinMax.java
+++ tp_jeu/players/PlayerMinMax.java
@@ -8,10 +8,11 @@ import tp_jeu.interfaces.Game.Move;
 
 /**
  * An implementation of {@link Player} that uses the MiniMax algorithm
+ * 
  * @author vdrevell
  *
  */
-public class PlayerMinMax implements Player {	
+public class PlayerMinMax implements Player {
 	/**
 	 * Infinity: +oo and -oo
 	 */
@@ -24,30 +25,38 @@ public class PlayerMinMax implements Player {
 		Move move;
 		double value;
 
-		public ValuedMove(Move move, double value) {this.move = move; this.value = value;}
-		public String toString() { return move.toString() + " (value = " + value + ")"; };
+		public ValuedMove(Move move, double value) {
+			this.move = move;
+			this.value = value;
+		}
+
+		public String toString() {
+			return move.toString() + " (value = " + value + ")";
+		};
 	}
 
 	/**
 	 * The maximum depth of the game tree to develop
 	 */
 	protected int maxDepth;
-	
+
 	/**
 	 * The evaluation function use to evaluate the game states
 	 */
 	protected EvaluationFunction evalFunc;
-		
+
 	/**
 	 * Default constructor, use the generic evaluation function.
+	 * 
 	 * @param maxDepth: maximum depth of game tree exploration.
 	 */
 	public PlayerMinMax(int maxDepth) {
 		this(maxDepth, new EvalWinLose());
 	}
-	
+
 	/**
 	 * Constructor with ability to set the maximum allowed computation time
+	 * 
 	 * @param maxDepth: maximum depth of game tree exploration.
 	 * @param evalFunc: the EvaluationFunction to use with the game
 	 */
@@ -55,22 +64,54 @@ public class PlayerMinMax implements Player {
 		this.maxDepth = maxDepth;
 		this.evalFunc = evalFunc;
 	}
-	
+
+	/**
+	 * Play a move using the MinMax algorithm
+	 * 
+	 * @param game the game to play
+	 * @return the best move
+	 */
 	@Override
 	public Move play(Game game) {
-		// TODO set the player of the evalFunction if needed
-		
-		// TODO call the minmax algorithm to get the best move and its value
-		ValuedMove bestMove = new ValuedMove(null, 0.0);
-		
+		evalFunc.setPlayer(game.player());
+		ValuedMove bestMove = minmax(game, 0);
+
 		System.out.println("MinMax choose " + bestMove);
 		return bestMove.move;
 	}
-	
-	
-	private ValuedMove minmax(Game game, int depth) { 
-		// TODO implement the minmax algorithm (add parameters if needed)
-		// Use "evalFunc.eval(game);" or "evalFunc.evalForPlayer(game, playerId);" to evaluate a game state
-		return new ValuedMove(null, 0.0);
+
+	private ValuedMove minmax(Game game, int depth) {
+		if (depth == maxDepth || game.possibleMoves().isEmpty()) {
+			return new ValuedMove(null, evalFunc.eval(game));
+		}
+
+		if (game.player() == Game.PlayerId.ONE) {
+			double maxValue = -oo;
+			Move bestMove = null;
+			for (Move move : game.possibleMoves()) {
+				Game gameCopy = game.clone();
+				gameCopy.play(move);
+				double value = minmax(gameCopy, depth + 1).value;
+				if (value > maxValue) {
+					maxValue = value;
+					bestMove = move;
+				}
+			}
+			return new ValuedMove(bestMove, maxValue);
+		} else {
+			double minValue = oo;
+			Move bestMove = null;
+			for (Move move : game.possibleMoves()) {
+				Game gameCopy = game.clone();
+				gameCopy.play(move);
+				double value = minmax(gameCopy, depth + 1).value;
+				if (value < minValue) {
+					minValue = value;
+					bestMove = move;
+				}
+			}
+			return new ValuedMove(bestMove, minValue);
+		}
+
 	}
 }
